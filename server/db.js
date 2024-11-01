de068db999d59e027ec0654900607e02be8483f4
Laboratório FIAP <labsfiap@fiap.com.br>
2024-11-01 13:38:21 -0300

    vercel3

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 const dbPassword = process.env.REACT_DB_PASSWORD;
 
 export default async function conectaNaDb(){
-    mongoose.connect(`mongodb+srv://duduzinhochorro:duduchor1407@spotifydb.feiux.mongodb.net/?retryWrites=true&w=majority&appName=spotifydb`);
+    mongoose.connect(`mongodb+srv://duduzinhochorro:duduchor1407@spotifydb.feiux.mongodb.net/test?retryWrites=true&w=majority&appName=spotifydb`);
 
     return mongoose.connection;
 }
