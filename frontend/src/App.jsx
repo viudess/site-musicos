907c293cb9501434fe89599870387581bb327ca9
Laboratório FIAP <labsfiap@fiap.com.br>
2024-11-01 13:32:53 -0300

    vercel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,17 @@ import Sidebar from "./components/Sidebar";
 import Container from "./components/Container";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
+import { useEffect } from "react";
 
 function App() {
+
+  useEffect(() => {
+      fetch('https://site-musicos-wl7h.vercel.app/artistas')
+      .then(res => res.json())
+      .then(data => {setArtistas(data), console.log(data)})
+      .catch(err => console.log(err))
+      .finally(() => setIsLoading(false))
+  },[])
 
   return (
     <>
